4be76e74706b0b2148024fe12ebc2aa3f7875477
arie <arieinjector@gmail.com>
2022-05-09 19:05:18 +0700

    add animation in service and showProject .jsx

--- src/components/home/ShowProject.jsx
+++ src/components/home/ShowProject.jsx
@@ -1,26 +1,26 @@
 const ShowProject = () => {
     return (
         <div className="font-pupylinux container mx-auto px-5 py-20">
-            <p className="text-[#3B71FE] font-semibold text-xl mb-4">the project i have made</p>
-            <p className="text-[#0E0E2C] font-bold text-3xl md:w-[600px] mb-8">projects built with peat and open source</p>
+            <p className="text-[#3B71FE] font-semibold text-xl mb-4" data-aos="fade-right" data-aos-duration="600" data-aos-delay="200">the project i have made</p>
+            <p className="text-[#0E0E2C] font-bold text-3xl md:w-[600px] mb-8" data-aos="fade-right" data-aos-duration="500" data-aos-delay="600">projects built with peat and open source</p>
             <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-y-14 md:gap-x-5 lg:gap-x-9 xl:gap-x-0 justify-items-center">
-                <div>
+                <div data-aos="fade-right" data-aos-duration="600" data-aos-delay="300">
                     <img src="/asset/kasir-comp.png" className="mb-3 rounded-lg w-[90vw] md:w-[350px]" alt="" />
                     <p className="text-[#3B71FE] font-bold text-lg">Kasir Comp</p>
                 </div>
-                <div>
+                <div data-aos="fade-down" data-aos-duration="600" data-aos-delay="300">
                     <img src="/asset/perpus.png" className="mb-3 rounded-lg w-[90vw] md:w-[350px]" alt="" />
                     <p className="text-[#3B71FE] font-bold text-lg">Perpus STM</p>
                 </div>
-                <div className="row-span-2">
+                <div className="row-span-2" data-aos="fade-left" data-aos-duration="600" data-aos-delay="500">
                     <img src="/asset/quran-app.png" className="mb-3 rounded-lg w-[90vw] md:w-[350px]" alt="" />
                     <p className="text-[#3B71FE] font-bold text-lg">Quran App</p>
                 </div>
-                <div>
-                    <img src="/asset/kasir-comp.png" className="mb-3 rounded-lg w-[90vw] md:w-[350px]" alt="" />
-                    <p className="text-[#3B71FE] font-bold text-lg">Kasir Comp</p>
+                <div data-aos="fade-right" data-aos-duration="600" data-aos-delay="300">
+                    <img src="/asset/goblog.png" className="mb-3 rounded-lg w-[90vw] md:w-[350px]" alt="" />
+                    <p className="text-[#3B71FE] font-bold text-lg">Go Blog</p>
                 </div>
-                <div>
+                <div data-aos="fade-up" data-aos-duration="600" data-aos-delay="300">
                     <img src="/asset/kasir-comp.png" className="mb-3 rounded-lg w-[90vw] md:w-[350px]" alt="" />
                     <p className="text-[#3B71FE] font-bold text-lg">Kasir Comp</p>
                 </div>
